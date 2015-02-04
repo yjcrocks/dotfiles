3fd3fdb7a6b2e629caa7054a6754ee985800d3e0
Youngjae Chang <caspian0207@gmail.com>
2015-02-04 17:58:20 +0900

    Configured Zsh and Oh-my-zsh

---
+++ zsh/path.zsh
@@ -0,0 +1,11 @@
+# pintos
+export PATH=$HOME/pintos/src/utils:$PATH
+
+# virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/project
+source /usr/local/bin/virtualenvwrapper.sh
+
+# renv
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
