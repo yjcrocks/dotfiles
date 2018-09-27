3c3ede6e4d1b2f99cf7bba10f37f7d5c5aded897
Youngjae Chang <caspian0207@gmail.com>
2018-09-27 15:15:31 +0100

    fix: do not use system packages on mkvv

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,7 +3,9 @@ alias cls=clear
 alias tmux='tmux -2'
 alias tma='tmux attach -t'
 
+alias reset_camera='sudo killall VDCAssistant'
+
 # Python related
 # alias python=python3
-alias mkvv='python3 -m venv venv --system-site-packages'
+alias mkvv='python3 -m venv venv'
 alias vv='source venv/bin/activate'
